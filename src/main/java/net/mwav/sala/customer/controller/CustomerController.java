16bb4935bdc3817904a23b4aa5ed417b3d66bb27
gregor77kr <7837167@naver.com>
2022-12-17 01:42:47 +0900

    Replace @autowired to constructor injection

--- src/main/java/net/mwav/sala/customer/controller/CustomerController.java
+++ src/main/java/net/mwav/sala/customer/controller/CustomerController.java
@@ -4,7 +4,6 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import net.mwav.sala.common.dto.StandardResponseBody;
 import net.mwav.sala.customer.dto.SignUpRequest;
 import net.mwav.sala.customer.dto.SignUpResponse;
@@ -20,15 +20,15 @@ import net.mwav.sala.customer.service.CustomerService;
 
 @RestController
 @RequestMapping(value = "customer")
+@RequiredArgsConstructor
 public class CustomerController {
 
-	@Autowired
-	private CustomerService customerService;
+	private final CustomerService customerService;
 
 	@PostMapping(value = "/sign-up", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> signUp(@Valid @RequestBody SignUpRequest signUpRequest) throws NoSuchAlgorithmException {
 		StandardResponseBody<SignUpResponse> response = StandardResponseBody
-				.success(customerService.signUp(signUpRequest));
+			.success(customerService.signUp(signUpRequest));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
--- src/main/java/net/mwav/sala/customer/service/CustomerService.java
+++ src/main/java/net/mwav/sala/customer/service/CustomerService.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.mwav.sala.customer.dto.SignUpRequest;
 import net.mwav.sala.customer.dto.SignUpResponse;
@@ -16,18 +16,18 @@ import net.mwav.sala.customer.entity.Customer;
 import net.mwav.sala.customer.repository.CustomerRepository;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class CustomerService {
 
-	@Autowired
-	private CustomerRepository customerRepository;
+	private final CustomerRepository customerRepository;
 
 	@Transactional
 	public SignUpResponse signUp(SignUpRequest signUpRequest) throws NoSuchAlgorithmException {
 		log.debug(signUpRequest.toString());
 
 		Optional<Customer> signed = customerRepository
-				.findOneByNameOrEmail(signUpRequest.getName(), signUpRequest.getEmail());
+			.findOneByNameOrEmail(signUpRequest.getName(), signUpRequest.getEmail());
 
 		if (signed.isPresent()) {
 			throw new DataIntegrityViolationException("이미 가입한 사용자입니다.");
