ca9ec05ba2bcd1d6129ff6ffa35cf7c7de4b42ab
gregor77kr <7837167@naver.com>
2023-02-10 17:07:17 +0900

    Remove a subscription item entity from Product domain

--- src/main/java/net/mwav/sala/product/service/ProductService.java
+++ src/main/java/net/mwav/sala/product/service/ProductService.java
@@ -1,16 +1,12 @@
 package net.mwav.sala.product.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.persistence.EntityNotFoundException;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import net.mwav.sala.product.entity.Product;
 import net.mwav.sala.product.repository.ProductRepository;
-import net.mwav.sala.subscription.entity.SubscriptionItem;
 
 @Service
 @RequiredArgsConstructor
@@ -18,21 +14,9 @@ public class ProductService {
 
 	private final ProductRepository productRepository;
 
-	public List<SubscriptionItem> getProductItems(List<SubscriptionItem> items) {
-		List<SubscriptionItem> copies = items.stream().collect(Collectors.toList());
-		
-		List<Product> products = productRepository
-				.findAllById(copies.stream().map(i -> i.getProduct().getId()).collect(Collectors.toList()));
-		copies.stream().forEach(i -> {
-			Product product = products.stream()
-					.filter(p -> p.getId() == i.getProduct().getId())
-					.findFirst()
-					.orElseThrow(EntityNotFoundException::new);
-
-			i.setProduct(product);
-		});
-		
-		return copies;
+	public List<Product> findProducts(List<Long> items) {
+		List<Product> products = productRepository.findAllById(items);
+		return products;
 	}
 
 }
--- src/main/java/net/mwav/sala/subscription/service/SubscriptionService.java
+++ src/main/java/net/mwav/sala/subscription/service/SubscriptionService.java
@@ -1,12 +1,15 @@
 package net.mwav.sala.subscription.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import net.mwav.sala.product.entity.Product;
 import net.mwav.sala.product.service.ProductService;
 import net.mwav.sala.subscription.entity.Subscription;
 import net.mwav.sala.subscription.entity.SubscriptionItem;
@@ -26,11 +29,26 @@ public class SubscriptionService {
 	public Subscription subscribe(Subscription subscription) {
 		// items의 product id로 가격을 조회하여 bind
 		List<SubscriptionItem> items = subscription.getItems();
-		items = productService.getProductItems(items);
-		
+		mapItemToProduct(items);
+
 		subscription.changeState(new CreatedState());
 		subscription = subscriptionRepository.save(subscription);
 		return subscription;
+	}
+
+	private void mapItemToProduct(List<SubscriptionItem> items) {
+		// items의 product id로 가격을 조회하여 bind
+		List<Long> ids = items.stream().map(i -> i.getProduct().getId()).collect(Collectors.toList());
+		List<Product> products = productService.findProducts(ids);
+
+		items.stream().forEach(i -> {
+			Product product = products.stream()
+					.filter(p -> p.getId() == i.getProduct().getId())
+					.findFirst()
+					.orElseThrow(EntityNotFoundException::new);
+
+			i.setProduct(product);
+		});
 	}
 
 	// 취소
